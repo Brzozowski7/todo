bf04d60e3fd69585f9d4863fa4b9ccf168746385
Krzysztof Brzozowski <krzysztofbrzozowski7@gmail.com>
2022-10-05 13:14:12 +0200

    configured emulator to work with cypress

--- src/backend/firebase.ts
+++ src/backend/firebase.ts
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import {
+  initializeFirestore,
+  connectFirestoreEmulator,
+} from "firebase/firestore";
 
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyAwK8L-Z36JJoY0kGVP5K8eepI0SMG9wno",
@@ -9,7 +12,9 @@ const firebaseApp = initializeApp({
   messagingSenderId: "319405141795",
   appId: "1:319405141795:web:bfe2b8fe5c110d4346a696",
 });
-const db = getFirestore(firebaseApp);
+const db = initializeFirestore(firebaseApp, {
+  experimentalForceLongPolling: true,
+});
 
 if (window.location.hostname === "localhost") {
   connectFirestoreEmulator(db, "localhost", 8080);
